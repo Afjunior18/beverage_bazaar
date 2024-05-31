7462f6405b180e7fbf92fe782d331e4a9433eee5
Afjunior18 junior <amauryjuniorvet@yahoo.com.br>
2024-05-31 12:06:01 +0000

    Create checkout.html, setting crispy forms

--- beverage_bazaar/settings.py
+++ beverage_bazaar/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'storages',
     'bag',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'beverage_bazaar.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -84,6 +87,9 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins':
+            'crispy_forms.templatetags.crispy_forms_tags',
+            'crispy_forms.templatetags.crispy_forms_field'
         },
     },
 ]

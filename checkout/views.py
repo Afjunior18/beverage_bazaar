a6385aeba3d0fe1f7b3ec26b228532eb7f3218a2
Afjunior18 junior <amauryjuniorvet@yahoo.com.br>
2024-06-10 10:38:36 +0000

    Add stripe elements, start styling checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PCemyP3MjpZMpQU9tpcUfI23I50vsL6DPKuK2ZIhMq2iHKla4SjFH200ruSiPLV2V6fCDbaDbqctw1xPgCFEmTH00X3SjJLPr',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

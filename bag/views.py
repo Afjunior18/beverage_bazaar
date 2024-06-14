d5a5a45059dc96b3e4c437353c7ef3740a26c531
Afjunior18 junior <amauryjuniorvet@yahoo.com.br>
2024-06-14 15:57:29 +0000

    Add a add to bag button in product page

--- bag/views.py
+++ bag/views.py
@@ -16,8 +16,8 @@ def add_to_bag(request, item_id):
 
     product = Product.objects.get(pk=item_id)
 
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+    quantity = int(request.POST.get('quantity', 1))
+    redirect_url = request.POST.get('redirect_url', '/products/')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):

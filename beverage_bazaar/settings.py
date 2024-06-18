e6042cd42ce3ad3e55e1149b056f296220a900fb
Afjunior18 junior <amauryjuniorvet@yahoo.com.br>
2024-06-18 12:02:03 +0000

    Remove secret key from setting, and set new one in env.py

--- beverage_bazaar/settings.py
+++ beverage_bazaar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g5o40%=667v4-tk42+lbc-rp7r*ro0l64866u_9s-zv!)d27i$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

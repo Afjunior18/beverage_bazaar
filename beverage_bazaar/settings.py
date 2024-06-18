0b6a57891ed4ef0a2476ebc8c45ed311b166a923
Afjunior18 junior <amauryjuniorvet@yahoo.com.br>
2024-06-18 20:19:18 +0000

    Deployment test

--- beverage_bazaar/settings.py
+++ beverage_bazaar/settings.py
@@ -14,13 +14,15 @@ from pathlib import Path
 
 import os
 import dj_database_url
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
+
 
 
 if os.path.exists("env.py"):
-  import env 
+  import env
+
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
+}
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -181,15 +183,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Cloudinary configuration
-# CLOUDINARY_URL = {
-#     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
-# }
-cloudinary.config( 
-  	cloud_name = os.environ.get("CLOUDINARY_CLOUD_NAME"),
-  	api_key = os.environ.get("CLOUDINARY_API_KEY"),
-  	api_secret = os.environ.get("CLOUDINARY_API_SECRET")
-)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/

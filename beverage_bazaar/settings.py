4a352a49b0a43229a1c1ed1e3dc641f51d7a52f4
Afjunior18 junior <amauryjuniorvet@yahoo.com.br>
2024-06-19 09:21:35 +0000

    Re config clouadinary for deployment

--- beverage_bazaar/settings.py
+++ beverage_bazaar/settings.py
@@ -14,14 +14,10 @@ from pathlib import Path
 
 import os
 import dj_database_url
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 
 
 if os.path.exists("env.py"):
   import env
-
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -182,24 +178,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Cloudinary configuration
-
-cloudinary.config( 
-  	cloud_name = os.environ.get("CLOUDINARY_CLOUD_NAME"),
-  	api_key = os.environ.get("CLOUDINARY_API_KEY"),
-  	api_secret = os.environ.get("CLOUDINARY_API_SECRET")
-)
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type

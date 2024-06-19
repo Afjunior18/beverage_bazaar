ffbcc73f1b4b3d899284962fa9b2f4a70b90fcf8
Afjunior18 junior <amauryjuniorvet@yahoo.com.br>
2024-06-19 21:42:10 +0000

    Add stripe var to heroku, turn debug to False, last deployment

--- beverage_bazaar/settings.py
+++ beverage_bazaar/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production
 DEBUG = False
 
 ALLOWED_HOSTS = [

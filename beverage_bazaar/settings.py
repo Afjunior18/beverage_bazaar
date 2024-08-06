8c782c22db85eb069b0d3f63433d7216662b5463
Afjunior18 junior <amauryjuniorvet@yahoo.com.br>
2024-08-06 14:12:30 +0000

    Create a env file to add secret information

--- beverage_bazaar/settings.py
+++ beverage_bazaar/settings.py
@@ -31,11 +31,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
                 '8000-afjunior18-beveragebaza-8k5ffklxuph.ws-eu114.gitpod.io',
-                'bevarages-bazaar-677df0f4b466.herokuapp.com'
+                'bevarages-bazaar-677df0f4b466.herokuapp.com',
+                '8000-afjunior18-beveragebaza-vybwjmo0xvw.ws-us115.gitpod.io'
                 ]
 
 
